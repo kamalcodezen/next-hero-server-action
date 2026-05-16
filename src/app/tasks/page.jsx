13dfb336e6596a1995a82ec1dee24c0fcb75c630
kamalcodezen <kamaluddin7908@gmail.com>
2026-05-16 23:05:07 +0530

    tasks card create

---
+++ src/app/tasks/page.jsx
@@ -0,0 +1,20 @@
+import TaskCard from "@/components/taskCard/TaskCard";
+import { getTask } from "@/lib/task";
+
+const TasksPages = async () => {
+  const tasks = await getTask();
+  console.log(tasks);
+
+  return (
+    <div>
+      <h2>Tasks : {tasks.length}</h2>
+      <div className="grid grid-cols-3 gap-6">
+        {tasks.map((task) => (
+          <TaskCard key={task.id} task={task}></TaskCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TasksPages;
---
+++ src/components/taskCard/TaskCard.jsx
@@ -0,0 +1,33 @@
+
+import { Card, Link } from "@heroui/react";
+
+
+
+const TaskCard = ({ task }) => {
+  return (
+    <Card variant="outline" className="border-2 border-zinc-600">
+      {/* <CircleDollar
+        aria-label="Dollar sign icon"
+        className="text-primary size-6"
+        role="img"
+      /> */}
+      <Card.Header>
+        <Card.Title>{task.title}</Card.Title>
+        <Card.Description>{task.description}</Card.Description>
+      </Card.Header>
+      <Card.Footer>
+        <Link
+          aria-label="Go to Acme Creator Hub (opens in new tab)"
+          href="https://heroui.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Creator Hub
+          <Link.Icon aria-hidden="true" />
+        </Link>
+      </Card.Footer>
+    </Card>
+  );
+};
+
+export default TaskCard;
